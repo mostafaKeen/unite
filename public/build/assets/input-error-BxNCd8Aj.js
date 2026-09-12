3d2cb6846ed96dbac74857b9d3a93a5d49f81584
mostafaKeen <mostafa@keenenter.com>
2026-09-12 14:46:15 +0300

    first commit 2

---
+++ public/build/assets/input-error-BxNCd8Aj.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-CcBQsCQo.js";import{t}from"./utils-CCDJ05Ll.js";var n=e();function r({message:e,className:r=``,...i}){return e?(0,n.jsx)(`p`,{...i,className:t(`text-sm text-red-600 dark:text-red-400`,r),children:e}):null}export{r as t};
