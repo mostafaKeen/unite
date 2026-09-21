e2b2d342a1bf6335f0cd51e6a74854539c2a3746
mostafaKeen <mostafa@keenenter.com>
2026-09-21 15:43:40 +0300

    items log

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Head, Link } from '@inertiajs/react';
 import { UniteHeader } from '@/components/UniteHeader';
 import { 
